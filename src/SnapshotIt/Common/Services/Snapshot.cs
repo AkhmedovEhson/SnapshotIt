20d570d5e7c551819b987acfdac4a7a77a3bdaa0
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-09-12 09:14:12 +0500

    🧵 feat: Simple Dictionary was replaced with `ConcurrentDictionary`, to use dictionary on Thread-Safe level.

--- src/SnapshotIt/Common/Services/Snapshot.cs
+++ src/SnapshotIt/Common/Services/Snapshot.cs
@@ -1,35 +1,39 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
 
 namespace SnapshotIt.Common.Services
 {
+    /// <summary> <seealso cref="Snap{T}"/> is mini-util, provides snapshot action </summary>
     public static class Snap<T> where T : class
     {
-        private static IDictionary<string, Stack<string>> Snapshots  = new Dictionary<string, Stack<string>>();
+        private static ConcurrentDictionary<string, Stack<string>> Snapshots  = new ConcurrentDictionary<string, Stack<string>>();
 
+        /// <summary>
+        /// <seealso cref="Pick(T)"/> is method, pushes the snapshot to ( 'cache' )
+        /// </summary>
         public static void Pick(T entity)
         {           
             string name = entity.GetType().Name;
             if (Snapshots.ContainsKey(name))
             {
-                Snapshots[name].Pop();
-                Snapshots[name].Push(JsonSerializer.Serialize<T>(entity));
+                Snapshots.GetValueOrDefault(name)?.Pop();
+                Snapshots.GetValueOrDefault(name)?.Push(JsonSerializer.Serialize<T>(entity));
                 return;
             }
 
             var stack = new Stack<string>(); 
             stack.Push(JsonSerializer.Serialize<T>(entity));
-            Snapshots.Add(name, stack);
-
+            Snapshots.TryAdd(name, stack);
         }
 
-        /// <summary> Method returns Entity if it's been snapshotted else <see cref="Exception"/></summary> 
+        /// <summary><seealso cref="ClapOne"/> Method returns Entity if it's been snapshotted else <see cref="Exception"/></summary> 
         public static T ClapOne()
         {
             string key = typeof(T).Name;
             
             if (Snapshots.ContainsKey(key))
             {
-                return JsonSerializer.Deserialize<T>(Snapshots[key].Pop())!;
+                return JsonSerializer.Deserialize<T>(Snapshots.GetValueOrDefault(key)!.Pop())!;
             }
             throw new Exception("No Snapshots");
         }
