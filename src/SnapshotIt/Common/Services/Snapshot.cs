ae7226bed5f99c9460b956d93c936dc7fae1c68a
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-09-17 17:50:05 +0500

    ♻️ perf: refactoring the `snapshot` service

--- src/SnapshotIt/Common/Services/Snapshot.cs
+++ src/SnapshotIt/Common/Services/Snapshot.cs
@@ -1,46 +1,35 @@
-﻿using System.Collections.Concurrent;
+﻿using SnapshotIt.Domain.Utils;
+using System.Collections.Concurrent;
 using System.Text.Json;
 
 namespace SnapshotIt.Common.Services
 {
+
     /// <summary> <seealso cref="Snap{T}"/> is mini-util, provides snapshot action </summary>
-    public static class Snap<T> where T : class
+    public class Snap<T> where T : class
     {
-        private static ConcurrentDictionary<string, Stack<string>> Snapshots  = new ConcurrentDictionary<string, Stack<string>>();
+        private Snaps<T> snapshots = new Snaps<T>();
 
         /// <summary>
         /// <seealso cref="Pick(T)"/> is method, pushes the snapshot to ( 'cache' )
         /// </summary>
-        public static void Pick(T entity)
-        {           
-            string name = entity.GetType().Name;
-            if (Snapshots.ContainsKey(name))
-            {
-                Snapshots.GetValueOrDefault(name)?.Pop();
-                Snapshots.GetValueOrDefault(name)?.Push(JsonSerializer.Serialize<T>(entity));
-                return;
-            }
-
-            var stack = new Stack<string>(); 
-            stack.Push(JsonSerializer.Serialize<T>(entity));
-            Snapshots.TryAdd(name, stack);
+        public void Pick(ref T entity)
+        {       
+            snapshots.Push(ref entity);
         }
 
         /// <summary><seealso cref="ClapOne"/> Method returns Entity if it's been snapshotted else <see cref="Exception"/></summary> 
-        public static T ClapOne()
+#nullable enable
+        public T? ClapOne()
         {
-            string key = typeof(T).Name;
-            
-            if (Snapshots.ContainsKey(key))
-            {
-                return JsonSerializer.Deserialize<T>(Snapshots.GetValueOrDefault(key)!.Pop())!;
-            }
-            throw new Exception("No Snapshots");
-        }
 
-        public static void Clear()
+            return snapshots.Get();
+            
+        }
+#nullable disable
+        public void Clear()
         {
-            Snapshots.Clear();
+            snapshots = new Snaps<T>();
         }
     }
 }
---
+++ src/SnapshotIt/Domain/Utils/Snaps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.Domain.Utils
+{
+
+    public struct Snaps<T>
+    {
+        private T[] captures = new T[1];
+
+        
+        public void Push(ref T entity)
+        {
+            captures[0] = entity;
+        }
+
+        public T Get()
+        {
+            if (captures[0].GetType().Name != typeof(T).Name)
+                return default(T);
+
+            return captures[0];
+        }
+
+        public Snaps(T[] captures)
+        {
+            this.captures ??= captures;
+        }
+    }
+
+
+}
