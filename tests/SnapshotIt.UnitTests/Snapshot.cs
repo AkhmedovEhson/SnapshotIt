51422cde06d7f447905543c1632eaceed046416b
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-09-25 11:46:51 +0500

    Up Tests

--- tests/SnapshotIt.UnitTests/Snapshot.cs
+++ tests/SnapshotIt.UnitTests/Snapshot.cs
@@ -18,23 +18,55 @@ namespace SnapshotIt.UnitTests
         [Test]
         public void Pick()
         {
-            var data = new Snap<o>(new o() { id = 2});
-            data.Should().NotBeNull();
-            data.ClapOne().Should().NotBeNull();
-            data.ClapOne().Should().BeOfType<o>();
+            var data = new Snap<o>(new o { id = 1 });
+            var apple = data.ClapOne();
 
+            apple.Should().NotBeNull();
+            apple.Should().BeOfType<o>();
+          
         }
 
         [Test]
         public void Pick2()
         {
-            var data = new Snap<o>(new o { id = 3 });
-            data.ClapOne()!.id.Should().Be(3);
+            var snapshot = new Snap<o>();
 
-            o _ = new o() { id = 1 };
-            data.Pick(ref _);
-            data.ClapOne()!.id.Should().Be(1);
+            for(int i = 1; i <= 5; i++)
+            {
+                snapshot.Pick(new o { id = i });    
+            }
+
+            
+            snapshot.snapshot_collection.Should().NotBeNull();
+            snapshot.snapshot_collection.Should().HaveCount(5);
+            snapshot.snapshot_collection.Should().NotContainNulls();
+
+            snapshot.ClapOne(3).Value.Should().NotBeNull();
+            snapshot.ClapOne(3).Value?.id.Should().Be(4);
+
+
         }
+
+        [Test]
+        public void ClapFail()
+        {
+            var snapshot = new Snap<o>();
+            snapshot.ClapOne().Value.Should().BeNull();
+            snapshot.ClapOne(2).Value.Should().BeNull();
+        }
+
+        [Test]
+        public void ClapSuccess()
+        {
+            var snapshot = new Snap<o>(new o { id = 2});
+            snapshot.ClapOne().Value.Should().NotBeNull();
+            snapshot.ClapOne().Value!.id.Should().Be(2); 
+            
+           
+        }
+ 
+
+
 
     }
 }
