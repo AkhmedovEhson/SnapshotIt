1eee9a15ec447736518c1083528583db29dcda95
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-09-25 11:44:10 +0500

    feat: New dto `SValue`

---
+++ src/SnapshotIt/Domain/Common/Types/SValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.Domain.Common.Types
+{
+    /// <summary>
+    /// <seealso cref="SValue{T}"/> is result type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct SValue<T>
+    {
+        public T Value { get; set; }
+
+    }
+}
