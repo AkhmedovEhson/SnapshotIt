3bf179d1517471c312faa87da7228b5c01354995
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-06 09:42:47 +0500

    feat: Properties reset on runtime

---
+++ src/SnapshotIt/Domain/Common/Reflection/Property.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.Domain.Common.Reflection
+{
+    public static class PropertyReflection
+    {
+        public static void SetProperties<T,T2>(T arg,ref T2 arg2)
+        {
+            var properties = arg.GetType().GetProperties();
+            var properties2 = arg2.GetType().GetProperties();
+
+            if (properties.Any())
+            {
+                for(int i = 0; i < properties.Length; i++)
+                {
+                    if (properties[i].Name == properties2[i].Name)
+                    {
+                        properties2[i].SetValue(arg2, properties[i].GetValue(arg));
+                    }
+                }
+            }
+        }
+    }
+}
