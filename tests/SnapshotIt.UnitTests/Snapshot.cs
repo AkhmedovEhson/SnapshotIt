233597dc99faf68e333e34c9c5c827c6289832a8
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-07 09:59:24 +0500

    Update Tests

--- tests/SnapshotIt.UnitTests/Snapshot.cs
+++ tests/SnapshotIt.UnitTests/Snapshot.cs
@@ -1,10 +1,9 @@
 using SnapshotIt;
-using SnapshotIt.Common.Services;
 using FluentAssertions;
 using SnapshotIt.Domain;
-using SnapshotIt.Common;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Attributes;
+using SnapshotIt.Domain.Utils;
 
 namespace SnapshotIt.UnitTests
 {
@@ -18,44 +17,45 @@ namespace SnapshotIt.UnitTests
         [Test]
         public void Pick()
         {
-            var data = new Snap<o>(new o { id = 1 });
-            var apple = data.ClapOne();
+            var data = new Snaps<o>(new o { id = 1 });
+            var apple = data.Get();
 
             apple.Should().NotBeNull();
-            apple.Value.id.Should().Be(1);
+            apple.Value.Value.id.Should().Be(1);
           
         }
 
         [Test]
         public void Pick2()
         {
-            var snapshot = new Snap<o>();
+            var snapshot = new Snaps<o>();
 
             for (int i = 0; i <= 100; i++)
             {
-                snapshot.Pick(new o { id = i });
+                snapshot.Push(new o { id = i });
             }
 
-            snapshot.ClapOne(4).Value.Should().NotBeNull();
-            snapshot.ClapOne(4).Value?.id.Should().Be(100);
+            snapshot.Get(4).Value.Should().NotBeNull();
+            snapshot.Get(4).Value?.id.Should().Be(100);
 
-            Assert.Throws<IndexOutOfRangeException>(() => snapshot.ClapOne(12));
+            Assert.Throws<IndexOutOfRangeException>(() => snapshot.Get(12));
         }
 
         [Test]
         public void ClapFail()
         {
-            var snapshot = new Snap<o>();
-            snapshot.ClapOne().Value.Should().BeNull();
-            snapshot.ClapOne(2).Value.Should().BeNull();
+            var snapshot = new Snaps<o>();
+            snapshot.Get().Value.Value.Should().BeNull();
+            snapshot.Get(2).Value.Should().BeNull();
         }
 
         [Test]
         public void ClapSuccess()
         {
-            var snapshot = new Snap<o>(new o { id = 2});
-            snapshot.ClapOne().Value.Should().NotBeNull();
-            snapshot.ClapOne().Value!.id.Should().Be(2); 
+            var snapshot = new Snaps<o>();
+            snapshot.Push(new o { id = 2 });
+            snapshot.Get()?.Value.Should().NotBeNull();
+            snapshot.Get()?.Value.id.Should().Be(2); 
             
            
         }
@@ -63,8 +63,8 @@ namespace SnapshotIt.UnitTests
         [Test]
         public void ClapThrowsException()
         {
-            var snapshot = new Snap<o>();
-            Assert.Throws<IndexOutOfRangeException> (() => snapshot.ClapOne(10));
+            var snapshot = new Snaps<o>();
+            Assert.Throws<IndexOutOfRangeException> (() => snapshot.Get(10));
         }
 
 
