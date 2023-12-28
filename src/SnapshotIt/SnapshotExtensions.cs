b7c887ef2328ed163acc3110387da9ca9d3834dd
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 10:13:45 +0500

    Provide `SnapshotExtensions`

---
+++ src/SnapshotIt/SnapshotExtensions.cs
@@ -0,0 +1,21 @@
+﻿using SnapshotIt.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt
+{
+    public static class SnapshotExtensions
+    {
+        public static IAsyncEnumerable<T> ReadFromBuffersLine<T>(this IAsyncLines _)
+        {
+            return Snaps<T>.ReadAllAsync();
+        }
+        public static void PostToBuffers<T>(this IAsyncLines _,T input)
+        {
+            Snaps<T>.Push(input);
+        }
+    }
+}
