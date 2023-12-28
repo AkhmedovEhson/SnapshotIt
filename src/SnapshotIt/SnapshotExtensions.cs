d86a02941e50f63c015221defc200572c72cfac1
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 11:29:16 +0500

    ♻️ Refactor `SnapshotExtensions`

--- src/SnapshotIt/SnapshotExtensions.cs
+++ src/SnapshotIt/SnapshotExtensions.cs
@@ -9,13 +9,9 @@ namespace SnapshotIt
 {
     public static class SnapshotExtensions
     {
-        public static IAsyncEnumerable<T> ReadFromBuffersLine<T>(this IAsyncLines _)
-        {
-            return Snaps<T>.ReadAllAsync();
-        }
-        public static void PostToBuffers<T>(this IAsyncLines _,T input)
-        {
-            Snaps<T>.Push(input);
-        }
+        public static IAsyncEnumerable<T> ReadFromBuffersLine<T>(this IAsyncLines _) => Snaps<T>.ReadAllAsync();
+        public static void PostToBuffers<T>(this IAsyncLines _,T input) => Snaps<T>.Push(input);
+        public static void CreateBufferBlock<T>(this IAsyncLines _) => Snaps<T>.CreateBufferBlock();
+        public static void CloseBufferBlock<T>(this IAsyncLines _) => Snaps<T>.CloseBuffersBlock();
     }
 }
