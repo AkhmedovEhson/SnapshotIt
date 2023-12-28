9474a4ec7470c25d58b31c845747d624fd9a5d95
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 09:59:29 +0500

    Support tests

--- tests/SnapshotIt.UnitTests/Snapshot.cs
+++ tests/SnapshotIt.UnitTests/Snapshot.cs
@@ -19,34 +19,11 @@ namespace SnapshotIt.UnitTests
         [Test]
         public async Task Pick()
         {
-            var data = new Snaps<o>(new o { id = 1 });
-            var response = data.Get();
-            response.Should().NotBeNull();
-            response!.Value.Value.id.Should().Be(1);
+            Snapshot.Out.Post(1234);
+            var cache = Snapshot.Out.Get<int>(0);
+            cache.Should().NotBe(null);
+            cache.Should().Be(1234);
           
-        }
-
-        [Test]
-        public async Task Pick2()
-        {
-            const int iter = 100;
-            var snapshot = new Snaps<o>();
-
-            for (int i = 0; i <= iter; i++)
-            {
-                snapshot.Push(new o { id = i });
-            }
-            var check1 = await snapshot.GetAsync(55);
-            check1.Should().NotBeNull();
-            check1.Value?.id.Should().Be(55);
-
-        }
-
-        [Test]
-        public async Task ThrowsOutOfRange()
-        {
-            var snapshot = new Snaps<o>();
-            Assert.ThrowsAsync<IndexOutOfRangeException>(async () => await snapshot.GetAsync(1));
         }
 
 
