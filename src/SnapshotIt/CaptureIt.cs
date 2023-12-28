923cc99c07493b03b923177fde91932caf581017
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 14:31:38 +0500

    Provide some comments to `CaptureIt`

--- src/SnapshotIt/CaptureIt.cs
+++ src/SnapshotIt/CaptureIt.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace SnapshotIt.Domain.Utils
 {
+    /// <summary>
+    /// <seealso cref="CaptureIt{T}" /> provides bunch of APIs to collect data
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public static class CaptureIt<T>
     {
         private static T[] collection = null;
         private static int index = 0;
         private static int size = 0;
 
+
+        /// <summary>
+        /// Gets data from captures by index, otherwise if provided index is out of range, throws <seealso cref="IndexOutOfRangeException"/>
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public static T Get(int ind)
         {
             if (ind < 0 || ind >= collection.Length)
@@ -22,6 +33,10 @@ namespace SnapshotIt.Domain.Utils
             return collection[ind];
         }
 
+        /// <summary>
+        /// Creates new collection of captures with provided size
+        /// </summary>
+        /// <param name="s"></param>
         public static void Create(int s)
         {
             collection = new T[s];
@@ -52,15 +67,26 @@ namespace SnapshotIt.Domain.Utils
         {
             collection = new T[!s.HasValue ? size : s.Value];
         }
-
+        /// <summary>
+        /// Responds collection of captures as <seealso cref="Span{T}"/>
+        /// </summary>
+        /// <returns></returns>
         public static Span<T> GetAsSpan()
         {
             return collection.AsSpan();
         }
+        /// <summary>
+        /// Responds collection of captures as <seealso cref="ReadOnlySpan{T}"/>
+        /// </summary>
+        /// <returns></returns>
         public static ReadOnlySpan<T> GetAsReadonlySpan()
         {
             return new ReadOnlySpan<T>(collection);
         }
+        /// <summary>
+        /// Responds collection of captures as <seealso cref="IEnumerable{T}"/>
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<T> GetAsEnumerable()
         {
             return collection.AsEnumerable();
