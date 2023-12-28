d2dc693d313f5533ee052137910be8ac48b1ea79
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 14:30:16 +0500

    Provided some comments to `SnapshotExtensions`

--- src/SnapshotIt/SnapshotExtensions.cs
+++ src/SnapshotIt/SnapshotExtensions.cs
@@ -7,11 +7,36 @@ using System.Threading.Tasks;
 
 namespace SnapshotIt
 {
+    /// <summary>
+    /// <seealso cref="SnapshotExtensions"/> - provides bunch of APIs to communicate with `BufferBlocks`, `Streams` so on ...
+    /// </summary>
     public static class SnapshotExtensions
     {
+        /// <summary>
+        /// Reads from `BufferBlock`, Stream asynchronously
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_"></param>
+        /// <returns></returns>
         public static IAsyncEnumerable<T> ReadFromBuffersLine<T>(this IAsyncLines _) => Snaps<T>.ReadAllAsync();
+        /// <summary>
+        /// Pushs data to `BufferBlock`
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_"></param>
+        /// <param name="input"></param>
         public static void PostToBuffers<T>(this IAsyncLines _,T input) => Snaps<T>.Push(input);
+        /// <summary>
+        /// Creates new `BufferBlock`
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_"></param>
         public static void CreateBufferBlock<T>(this IAsyncLines _) => Snaps<T>.CreateBufferBlock();
+        /// <summary>
+        /// Closes `BufferBlock`, makes it completed, `BufferBlock`'ll not be able to get some data into, anymore.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_"></param>
         public static void CloseBufferBlock<T>(this IAsyncLines _) => Snaps<T>.CloseBuffersBlock();
     }
 }
