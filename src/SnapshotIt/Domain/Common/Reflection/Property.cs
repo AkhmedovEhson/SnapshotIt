4b9da82b1249cd124e6e35fb77d33a8aea4b79e6
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-24 09:16:38 +0500

    Some comments upon `SetProperties` </>

--- src/SnapshotIt/Domain/Common/Reflection/Property.cs
+++ src/SnapshotIt/Domain/Common/Reflection/Property.cs
@@ -4,6 +4,12 @@ namespace SnapshotIt.Domain.Common.Reflection
 {
     public static class PropertyReflection
     {
+        // Note: Complexity `O(n)`
+        // Please provide your decisions about it, if there is a way to improve complexity, just pull your request.
+        /// <summary>
+        /// Fills properties of component dynamically.
+        /// <br/>Argument by reference <paramref name="arg2"/>, so all changes effact to original variable(arg2)
+        /// </summary>
         public static void SetProperties<T,T2>(T arg,ref T2 arg2)
         {
             var properties = arg.GetType().GetProperties();
