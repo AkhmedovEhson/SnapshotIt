8f7f9ffb70b835a9f1e30c963b4dc3272c8b16e3
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-30 09:35:14 +0500

    Support tests

---
+++ tests/SnapshotIt.DependencyInjection.UnitTests/RuntimeRegisterServicesTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using SnapshotIt.DependencyInjection.UnitTests.TestObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.DependencyInjection.UnitTests
+{
+    public class RuntimeRegisterServicesTests
+    {
+        private ServiceCollection dep_collection = new ServiceCollection();
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var runtime = new RuntimeRegisterServices(Assembly.GetExecutingAssembly(),dep_collection);
+            runtime.ConfigureAllServices(Store.Transient);
+        }
+
+        [Test]
+        public void GetRegisteredServicesInDICollection_Success()
+        {
+            dep_collection.Should().NotBeNull();
+
+            // Note: should be `1` because there is only one class impl. SnapshotIt's interface on this assembly 
+            dep_collection.Count.Should().Be(1); 
+            dep_collection[0].Lifetime.Should().Be(ServiceLifetime.Transient);
+        }
+    }
+}
