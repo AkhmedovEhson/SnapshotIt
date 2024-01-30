d813139a345dc989f2a361d86654d6df1032ad75
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-30 09:36:02 +0500

    Objects, are used especially for testing perpose

---
+++ tests/SnapshotIt.DependencyInjection.UnitTests/TestObjects/CarObject.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.DependencyInjection.UnitTests.TestObjects
+{
+    public class Car:IRuntimeDependencyInjectionObject
+    {
+        public int Id { get; set; } 
+    }
+    public interface ICar { }
+}
