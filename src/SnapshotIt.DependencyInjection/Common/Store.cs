d7066970ef0faad640c6275f966bcc3d38c4b181
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-01-30 09:34:45 +0500

    Provided new feature, registers services on runtime

---
+++ src/SnapshotIt.DependencyInjection/Common/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.DependencyInjection;
+
+public enum Store
+{
+    Transient,
+    Scoped,
+    Singleton
+}
---
+++ src/SnapshotIt.DependencyInjection/RuntimeRegisterServices.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.DependencyInjection
+{
+    public class RuntimeRegisterServices
+    {
+        private Assembly ExecutingAssembly { get; set; }
+        private IServiceCollection ServiceCollection { get; set; }
+        public RuntimeRegisterServices(Assembly assembly,IServiceCollection services)
+        {
+            this.ExecutingAssembly = assembly;
+            this.ServiceCollection = services;
+        }
+
+        public void ConfigureAllServices(Store store)
+        {
+            var type = typeof(IRuntimeDependencyInjectionObject);
+            var types = ExecutingAssembly.GetExportedTypes().Where(o => type.IsAssignableFrom(o) && o.IsClass).ToList();
+
+            switch (store)
+            {
+                case Store.Transient:
+                    types.ForEach(o => ServiceCollection.AddTransient(o));
+                    break;
+                case Store.Scoped:
+                    types.ForEach(o => ServiceCollection.AddScoped(o));
+                    break;
+                default:
+                    types.ForEach(o => ServiceCollection.AddSingleton(o));
+                    break;
+
+            }
+        }
+
+    }
+}
