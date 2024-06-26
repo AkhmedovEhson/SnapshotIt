579e7bfb8471394e851b12088d89383d3555eaaa
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-06-26 14:50:21 +0500

    WIP: `ValidateAndPost` - performes operation of validating before posting to collection of captures

--- src/SnapshotIt.FluentValidations/FluentValidation.cs
+++ src/SnapshotIt.FluentValidations/FluentValidation.cs
@@ -1,7 +1,34 @@
-﻿namespace SnapshotIt.FluentValidations
-{
-    public class Class1
-    {
+﻿using SnapshotIt.FluentValidations;
+using FluentValidation;
+using System.Reflection;
+using System;
 
+namespace SnapshotIt.FluentValidations
+{
+    public static class FluentValidationAPIs
+    {
+        // TODO: It is required to refactor all stuff here ....
+        public static void ValidateAndPost<T>(this ISnapshot _,T obj)
+        {
+
+            Assembly assembly = FluentValidationAssembly.Assembly 
+                ?? Assembly.GetExecutingAssembly();
+
+            Type? validatorType = AssemblyScanner.FindValidatorsInAssembly(assembly)
+                .Select(o => o.ValidatorType)
+                .Where(o => o.IsSubclassOf(typeof(AbstractValidator<T>)))
+                .FirstOrDefault();
+
+            IValidator? validator = (IValidator?)Activator.CreateInstance(validatorType);
+            var context = new ValidationContext<T>(obj);
+            var response = validator!.Validate(context);
+
+            if (!response.IsValid)
+            {
+                throw new ArgumentException($"Input '{obj.GetType().FullName}' is not valid");
+            }
+
+            Snapshot.Out.Post<T>(obj);
+        }
     }
 }
---
+++ src/SnapshotIt.FluentValidations/FluentValidationAssembly.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotIt.FluentValidations
+{
+    /// <summary>
+    /// Adjusts assemblies
+    /// </summary>
+    public static class FluentValidationAssembly
+    {
+        /// <summary>
+        /// The `assembly` where the `ValidationContext` will be searched.
+        /// </summary>
+        public static Assembly? Assembly { get; private set; }
+        /// <summary>
+        /// Changes the current assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        public static void ConfigureAssembly(Assembly assembly) => Assembly = assembly;
+    }
+}
