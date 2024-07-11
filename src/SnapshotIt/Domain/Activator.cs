667b0794d8d1219de21422ff63307aefae4e1848
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2024-07-11 09:58:34 +0500

    FEAT: Added `ActivatorIt`, with extensions same as `System.Activator`

---
+++ src/SnapshotIt/Domain/Activator.cs
@@ -0,0 +1,13 @@
+﻿using SnapshotIt.Domain.Common.Reflection;
+
+namespace SnapshotIt.Domain;
+
+public static class ActivatorIt
+{
+    public static T CreateInstanceWithCopyingProperties<T>(T instance)
+    {
+        var copy = Activator.CreateInstance<T>();
+        PropertyReflection.SetProperties(instance, ref copy);
+        return copy;
+    }
+}
