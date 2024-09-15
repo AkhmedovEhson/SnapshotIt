cb3e6522a026cb959758516e8881e0450c7c18ab
Ehson <129420305+AkhmedovEhson@users.noreply.github.com>
2024-09-15 20:39:19 +0500

    REFACTOR: Got rid of stupid stuff ....

--- src/SnapshotIt/Snaps.cs
+++ src/SnapshotIt/Snaps.cs
@@ -17,16 +17,6 @@ namespace SnapshotIt.Domain.Utils
     public static class Snaps<T>
     {
         private static BufferBlock<T> _buffer = new();
-
-        // Note: creates default buffer-block if it is not created :)
-        static Snaps()
-        {
-            if (_buffer is null)
-            {
-                CreateBufferBlock();
-            }
-        }
-
         public static void Push(in T entity) => _buffer.Post<T>(entity);
         public static IAsyncEnumerable<T> ReadAllAsync() => _buffer.ReceiveAllAsync();
         public static void CloseBuffersBlock() => _buffer.Complete();
