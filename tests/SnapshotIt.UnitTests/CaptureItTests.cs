1a4df8ba0accdef24db3ee8014241fc8998d9be8
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2025-08-05 00:06:55 +0500

    REFACTOR: Tests are updated, to make it more readable

--- tests/SnapshotIt.UnitTests/CaptureItTests.cs
+++ tests/SnapshotIt.UnitTests/CaptureItTests.cs
@@ -16,13 +16,14 @@ namespace SnapshotIt.UnitTests
     {
         private const int _defaultSizeOfSnapshots = 100;
         [SetUp]
-        public void RunBeforeAnyTests()
+        public void RunBeforeAllTests()
         { 
            
             Snapshot.Out.Create<o>(_defaultSizeOfSnapshots);
         }
+
         [Test]
-        public void Capture_Post_Successfully()
+        public void Post_Successfully()
         {
             // Arrange
             var obj = new o()
@@ -39,7 +40,7 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void Captures_ClearDefinedType_Successfully()
+        public void Clear_ClearDefinedType_Successfully()
         {
             Assert.DoesNotThrow(() =>
             {
@@ -55,7 +56,7 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void Captures_ClearNotFoundType_Throws_ArgumentNullReferenceException()
+        public void Clear_ClearNotFoundType_Throws_ArgumentNullReferenceException()
         {
         
             Assert.Throws<ArgumentNullException>(() =>
@@ -65,7 +66,7 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void Captures_ClearAll_Successfully()
+        public void ClearAll_Successfully()
         {
             Snapshot.Out.Create<decimal>(10);
 
@@ -102,151 +103,154 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void Captures_ClearAll_Successfully()
+        public async Task GetAsync_Full_Success()
         {
-            Snapshot.Out.Create<decimal>(10);
-
-            for(int i = 0; i < 10; i++)
+            // Arrange
+            var @snapshot = new o[1] 
             {
-                Snapshot.Out.Post<decimal>(i);
-            }
-
-            Snapshot.Out.Create<short>(20);
-
-            for (int i = 0; i < 20; i++)
-            {
-                Snapshot.Out.Post<short>((short)i);
-            }
-
-            Snapshot.Out.Clear(); // WIP: Clears all sets ...
-
-            Assert.Throws<NullReferenceException>(() =>
-            {
-                Snapshot.Out.Get<decimal>(0);
-            });
-
-      
-            Assert.Throws<NullReferenceException>(() =>
-            {
-                Snapshot.Out.Get<short>(0);
-            });
-
-
-            Assert.Throws<NullReferenceException>(() =>
-            {
-                Snapshot.Out.Get<o>(0);
-            });
-        }
-
-        [Test]
-        public async Task CapturedObject_GetAsync_Success()
-        {
-            var obj = new o[] 
-            {
-                new o()
+                new()
                 {
                     id = 1
                 } 
             };
   
-            await Snapshot.Out.PostAsync<o>(obj);
-            var result = await Snapshot.Out.GetAsync<o>(0);
+            // Act
+            await Snapshot.Out.PostAsync<o>(snapshot);
+            var @object = await Snapshot.Out.GetAsync<o>(0);
 
-            result.Should().NotBeNull();
-            result.id.Should().Be(1);
+            @object.Should().NotBeNull();
+            @object.id.Should().Be(1);
+
+            // Assert
+            var other_objects = Snapshot.Out.GetAsList<o>()[1..].AsEnumerable();
+            other_objects.Should().AllSatisfy(e =>
+            {
+                e.Should().BeNull();
+            });
+                       
         }
 
         [Test]
-        public async Task CapturedObject_PostAsync_GetAllAsync_ArrayTimesToTwo_Success()
+        public async Task GetAsync_Success()
         {
-            var obj = new o[]
+            // Arrange
+            var @snapshot = new o[1]
             {
-                new o() { id = 1 },
-                new o() { id = 2 },
-                new o() { id = 3 }
-            };
-
-            Snapshot.Out.Create<o>(2);
-            await Snapshot.Out.PostAsync<o>(obj);
-            var result = await Snapshot.Out.GetAllAsync<o>();
-
-            result.Should().NotBeNull();
-            result.Length.Should().Be(4);
-            result[0].id.Should().Be(1);
-            result[1].id.Should().Be(2);
-            result[2].id.Should().Be(3);
-        }
-        [Test]
-        public async Task CapturedObject_PostAsync_GetAllAsync_Success()
-        {
-            var obj = new o[2]
-            {
-                new o()
+                new()
                 {
                     id = 1
-                },
-                new o()
-                {
-                    id = 2
                 }
             };
-            Snapshot.Out.Create<o>(2);
-            await Snapshot.Out.PostAsync<o>(obj);
-            var result = await Snapshot.Out.GetAllAsync<o>();
 
-            result.Should().NotBeNull();
-            result.Length.Should().Be(2);
-            result[0].id.Should().Be(1);
-            result[1].id.Should().Be(2);
+            // Act
+            await Snapshot.Out.PostAsync<o>(snapshot);
+            var @object = await Snapshot.Out.GetAsync<o>(0);
+
+            // Assert
+            @object.Should().NotBeNull();
+            @object.id.Should().Be(1);
         }
 
         [Test]
-        public void CapturedObject_Throws_NullReferenceException_Expression()
+        public async Task PostAsync_GetAllAsync_ArrayTimesToTwo_Success()
         {
+            // Arrange
+            var obj = new int[4]
+            {
+                1,2,3,4
+            };
 
+            Snapshot.Out.Create<int>(3);
+            var snapshots = await Snapshot.Out.GetAllAsync<int>();
+
+            snapshots.Should().NotBeNull();
+            snapshots.Length.Should().Be(3);
+
+            // Act
+            await Snapshot.Out.PostAsync<int>(obj);
+            var result = await Snapshot.Out.GetAllAsync<int>();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Length.Should().Be(6);
+
+        }
+        [Test]
+        public async Task PostAsync_GetAllAsync_Success()
+        {
+            // Arrange
+            var snapshots = new int[2]
+            {
+                1,2
+            };
+            Snapshot.Out.Create<int>(2);
+
+            // Act
+            await Snapshot.Out.PostAsync<int>(snapshots);
+            var result = await Snapshot.Out.GetAllAsync<int>();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Length.Should().Be(2);
+            result[0].Should().Be(1);
+            result[1].Should().Be(2);
+        }
+
+        [Test]
+        public void SnapshotNotFound_Throws_NullReferenceException_Expression()
+        {
+            // Arrange && Act
             Snapshot.Out.Post<o>(new o() { id = 1});
 
+            // Assert
             Assert.Throws<NullReferenceException>(() => Snapshot.Out.Get<o>(i => i.id == 2));
         }
 
         [Test]
-        public void CapturedObject_SuccessfullyFound_Expression()
+        public void SnapshotFound_Expression_Success()
         {
+            // Arrange
             Snapshot.Out.Post<o>(new o() { id = 1 });
 
+            // Act
             var result = Snapshot.Out.Get<o>(i => i.id == 1);
+
+            // Assert
             result.Should().NotBeNull();
             result.id.Should().Be(1);
         }
 
         [Test]
-        public void CapturedObject_NullResponseFromCapturedList_Sucessfully()
+        public void NotFound_ByIndex_RespondNull()
         {
-
+            // Arrange && Act
             var result = Snapshot.Out.Get<o>(0);
+
+            // Assert
             result.Should().BeNull();
         }
 
         [Test]
-        public void CapturedObject_Throws_IndexOutOfRangeException()
+        public void SnapshotByIndex_Throws_IndexOutOfRangeException()
         {
-            Snapshot.Out.Create<o>(1);
-            var collection = Snapshot.Out.GetAsEnumerable<o>();
-            collection.Count().Should().Be(1);
-            Snapshot.Out.Post(new o() { id = 1 });
+            // Arrange
+            Snapshot.Out.Create<int>(1);
+            var @enumerable = Snapshot.Out.GetAsEnumerable<int>();
+            @enumerable.Count().Should().Be(1);
 
+            // Act
+            Snapshot.Out.Post(1);
+
+            // Assert
             Assert.Throws<IndexOutOfRangeException>(() => {
-                var item = Snapshot.Out.Get<o>(3); // out of size
+                var item = Snapshot.Out.Get<int>(3); // out of size
             });
         }
 
         [Test]
-        public void CollectionOfCaptures_GetAsSpan_Successfully()
+        public void Snapshots_GetAsSpan_Successfully()
         {
-            // Arrange
-            Snapshot.Out.Post(new o { id = 123 });
-
-            // Act
+            // Arrange && Act
             var span = Snapshot.Out.GetAsSpan<o>();
 
 
@@ -257,13 +261,9 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void CollectionOfCaptures_GetAsEnumerable_Successfully()
+        public void Snapshots_GetAsEnumerable_Successfully()
         {
-
-            // Arrange
-            Snapshot.Out.Post(new o { id = 123 });
-
-            // Act
+            // Arrange && Act
             var enumerable = Snapshot.Out.GetAsEnumerable<o>();
 
             // Assert
@@ -272,18 +272,15 @@ namespace SnapshotIt.UnitTests
         }
 
         [Test]
-        public void CollectionOfCaptures_GetAsReadonlySpan_Successfully()
+        public void Snapshots_GetAsReadonlySpan_Successfully()
         {
-            // Arrange
-            Snapshot.Out.Post(new o { id = 123 });
-
-            // Act
-            var span = Snapshot.Out.GetAsReadonlySpan<o>();
+            // Arrange && Act
+            var readonly_span = Snapshot.Out.GetAsReadonlySpan<o>();
 
             // Assert
-            bool result = span is ReadOnlySpan<o>;
+            bool result = readonly_span is ReadOnlySpan<o>;
             result.Should().BeTrue();
-            span.Length.Should().Be(_defaultSizeOfSnapshots);
+            readonly_span.Length.Should().Be(_defaultSizeOfSnapshots);
         }
     }
 }
